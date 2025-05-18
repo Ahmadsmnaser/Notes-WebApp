df75adf6d5e89a98af4621552780c2d4504b7454
Ahmad Naser <157128325+Ahmadsmnaser@users.noreply.github.com>
2025-05-18 20:49:01 +0300

    Update test.spec.ts

--- frontend/playwright-tests/test.spec.ts
+++ frontend/playwright-tests/test.spec.ts
@@ -1,7 +1,6 @@
-// frontend/playwright-tests/test.spec.ts
 import { test, expect } from '@playwright/test';
 
-test('Home page has expected title', async ({ page }) => {
+test('homepage has welcome text', async ({ page }) => {
     await page.goto('http://localhost:3000');
-    await expect(page).toHaveTitle(/Notes/i);
+    await expect(page.getByText('Welcome to My Notes App')).toBeVisible();
 });
