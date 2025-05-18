18868d160e0fa09ebb2a6a68ee537baf8f17972e
Ahmad Naser <157128325+Ahmadsmnaser@users.noreply.github.com>
2025-05-18 22:29:22 +0300

    Update test.spec.ts

--- frontend/playwright-tests/test.spec.ts
+++ frontend/playwright-tests/test.spec.ts
@@ -2,13 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('homepage should have title', async ({ page }) => {
     await page.goto('http://localhost:3000');
-    await expect(page).toHaveTitle(/Vite/i);
+    await expect(page).toHaveTitle(/Notes/i);
 });
 
 test('homepage should display welcome text', async ({ page }) => {
     await page.goto('http://localhost:3000');
 
-    const welcomeText = 'Welcome to My Notes App';
-
-    await expect(page.getByText(welcomeText)).toBeVisible({ timeout: 10000 });
+    const welcomeText = page.getByRole('heading', { name: 'Welcome to My Notes App' });
+    await expect(welcomeText).toBeVisible({ timeout: 15000 });
 });
