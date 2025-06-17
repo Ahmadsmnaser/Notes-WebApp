49a6583190426a2dd0956dc36ceb8370b9efdf18
Ahmad Naser <Ahmadsmnaser@gmail.com>
2025-06-17 19:30:53 +0300

    hw3 submission

--- frontend/playwright-tests/test.spec.ts
+++ frontend/playwright-tests/test.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-const BASE_URL = 'http://localhost:5173';
+const BASE_URL = 'http://localhost:3000';
 
 const timestamp = Date.now();
 const testUser = {
